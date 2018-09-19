25cb22922c40cc46f844582d4189aff6f8889c73
Dominik Schroeck <dominik.schroeck@transentis.com>
2018-09-19 17:30:40 +0200

    Works now

--- BPTK_Py/abm/simultaneousScheduler.py
+++ BPTK_Py/abm/simultaneousScheduler.py
@@ -1,19 +1,17 @@
 #                                                       /`-
 # _                                  _   _             /####`-
-#| |                                | | (_)           /########`-
-#| |_ _ __ __ _ _ __  ___  ___ _ __ | |_ _ ___       /###########`-
-#| __| '__/ _` | '_ \/ __|/ _ \ '_ \| __| / __|   ____ -###########/
-#| |_| | | (_| | | | \__ \  __/ | | | |_| \__ \  |    | `-#######/
+# | |                                | | (_)           /########`-
+# | |_ _ __ __ _ _ __  ___  ___ _ __ | |_ _ ___       /###########`-
+# | __| '__/ _` | '_ \/ __|/ _ \ '_ \| __| / __|   ____ -###########/
+# | |_| | | (_| | | | \__ \  __/ | | | |_| \__ \  |    | `-#######/
 # \__|_|  \__,_|_| |_|___/\___|_| |_|\__|_|___/  |____|    `- # /
 #
 # Copyright (c) 2018 transentis labs GmbH
 # MIT License
 
 
-
-from ..logger import log
-
 from .scheduler import Scheduler
+from ..logger import log
 
 
 #################################
@@ -41,7 +39,6 @@ class SimultaneousScheduler(Scheduler):
         for sim_round in range(model.starttime, model.stoptime + 1):
 
             for step in range(round(1 / model.dt)):
-
                 self.run_step(model, sim_round, step, progress_widget)
 
     def run_step(self, model, sim_round, dt, progress_widget=None):
