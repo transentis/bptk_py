a79b21ad8127b2c1ff4a5b94633f69b2e29aa58d
Oliver Grasl <oliver.grasl@transentis.com>
2019-01-28 18:56:56 +0100

    Tidied the paths.

--- conf.py
+++ conf.py
@@ -15,9 +15,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('./BPTK_Py/abm'))
-#sys.path.insert(0, os.path.abspath('./BPTK_Py/systemdynamics'))
-#sys.path.insert(0, os.path.abspath('./BPTK_Py/logger'))
 
 # -- Project information -----------------------------------------------------
 
