a98f5d769ccb24d13ceae45c620dd025a61102eb
Oliver Grasl <oliver.grasl@transentis.com>
2019-03-10 14:42:29 +0100

    Updated the version flag.

--- conf.py
+++ conf.py
@@ -23,9 +23,9 @@ copyright = '2019, transentis labs GmbH'
 author = 'transentis labs GmbH'
 
 # The short X.Y version
-version = '0.6.1'
+version = '0.6.3'
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.6.3'
 
 
 # -- General configuration ---------------------------------------------------
