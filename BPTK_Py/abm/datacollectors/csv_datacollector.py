808b74f7be292b6bc984bf00326f916494c764ac
Dominik Schroeck <dominik.schroeck@transentis.com>
2019-03-14 14:39:59 +0100

    Added a Kinesis and CSV Data Collector

---
+++ BPTK_Py/abm/datacollectors/csv_datacollector.py
@@ -0,0 +1,101 @@
+#                                                       /`-
+# _                                  _   _             /####`-
+# | |                                | | (_)           /########`-
+# | |_ _ __ __ _ _ __  ___  ___ _ __ | |_ _ ___       /###########`-
+# | __| '__/ _` | '_ \/ __|/ _ \ '_ \| __| / __|   ____ -###########/
+# | |_| | | (_| | | | \__ \  __/ | | | |_| \__ \  |    | `-#######/
+# \__|_|  \__,_|_| |_|___/\___|_| |_|\__|_|___/  |____|    `- # /
+#
+# Copyright (c) 2018 transentis labs GmbH
+# MIT License
+
+
+#########################
+## DATACOLLECTOR CLASS ##
+#########################
+
+
+import csv
+
+
+class CSVDataCollector:
+    """
+    A datacollector for the agent based simulation.
+    Collects the output data of each agent/event and outputs them to CSV
+    For now it only outputs the agent statistics, not the event statistics
+    """
+
+    def __init__(self,filename):
+        """
+
+        :param filename: filename of output file
+        """
+        self.agent_statistics = {}
+        self.event_statistics = {}
+
+
+        self.column_names = None
+
+        self.outfile = open(filename, "w", encoding="UTF-8")
+        self.writer = csv.writer(self.outfile,delimiter=";")
+        self.headlines = None
+
+    def record_event(self, time, event):
+        """
+        Record an event
+        :param time: t (int)
+        :param event: event instance
+        :return: None
+        """
+        if time not in self.event_statistics:
+            self.event_statistics[time] = {}
+
+        if event.name not in self.event_statistics[time]:
+            self.event_statistics[time][event.name] = 0
+
+        self.event_statistics[time][event.name] += 1
+
+
+
+    def reset(self):
+        self.agent_statistics = {}
+
+    def collect_agent_statistics(self, sim_time, agents):
+        """
+        Collect agent statistics from agent(s)
+        :param sim_time: t (int)
+        :param agents: list of Agent
+        :return: None
+        """
+
+        for agent in agents:
+
+            stats = {}
+            stats["id"] = agent.id
+            stats["time"] = sim_time
+
+
+            for agent_property_name, agent_property_value in agent.properties.items():
+
+                stats[agent_property_name] = agent_property_value['value']
+
+
+            if not self.headlines:
+                 self.headlines = list(stats.keys())
+                 self.writer.writerow(self.headlines)
+
+            self.writer.writerow(list(stats.values()))
+            self.outfile.flush()
+
+
+
+
+    def statistics(self):
+        """
+        Get the statistics collected
+        :return: Dictionary
+        """
+
+        return {}
+
+
