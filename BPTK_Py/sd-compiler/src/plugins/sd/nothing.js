d5ff1cf9bd06ce6521fe1d2cd3df129c41abad88
Dominik Schroeck <dominik.schroeck@transentis.com>
2019-09-26 13:53:47 +0200

    Remove static SD Compiler

--- BPTK_Py/sd-compiler/src/plugins/sd/nothing.js
+++
@@ -1,9 +0,0 @@
-/*      _                   _ _
-  _____| |__ ___ _ __  _ __(_| |___ _ _
- (_-/ _` / _/ _ | '  \| '_ | | / -_| '_|
- /__\__,_\__\___|_|_|_| .__|_|_\___|_|
-                      |_|
- Copyright (c) 2013-2015 transentis management & consulting. All rights reserved.
-*/
-
-export default IR => Promise.resolve( IR )
