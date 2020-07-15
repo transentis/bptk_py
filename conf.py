283db710ab18748762f86c12a5ee1ed552954da5
Dominik Schroeck <dominik.schroeck@transentis.com>
2020-07-15 10:05:06 +0200

    1.1.9

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ author = 'transentis labs GmbH'
 # The short X.Y version
 version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.1.8'
+release = '1.1.9'
 
 
 # -- General configuration ---------------------------------------------------
