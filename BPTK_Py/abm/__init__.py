da7d25625f4ae56249c350e91cc3c7d1d49edcae
Stefanie Schroeck <stefanie.schroeck@transentis.com>
2020-09-09 17:41:25 +0200

    new data collector

--- BPTK_Py/abm/__init__.py
+++ BPTK_Py/abm/__init__.py
@@ -1,6 +1,7 @@
 from .agent import Agent
 from .dataCollector import DataCollector
 from .datacollectors import CSVDataCollector
+from .datacollectors import ExtendedDataCollector
 from .event import Event
 from .model import Model
 from .scheduler import Scheduler
