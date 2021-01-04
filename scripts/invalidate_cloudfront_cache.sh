1bd73de8060f2f1c2d994a8b5782831f2764f102
Oliver Grasl <oliver.grasl@transentis.com>
2021-01-04 20:30:30 +0100

    move documentation to bptk.transentis.com

--- scripts/invalidate_cloudfront_cache.sh
+++ scripts/invalidate_cloudfront_cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # only use this when it is urgend to get the docs updated. regular update time is 24h
-aws cloudfront create-invalidation --distribution-id EMYYIG5K526NL --paths "/*"
+aws cloudfront create-invalidation --distribution-id E3LWSA9IT755ZP --paths "/*" --profile bptk-website
