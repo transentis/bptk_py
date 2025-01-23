db7319d204cb63fb6fe08236d984e5e5a26c0254
mbaehring <markus.baehring@transentis.com>
2025-01-23 11:10:27 +0100

    added unittests for element.py

--- tests/unittests/test_element.py
+++ tests/unittests/test_element.py
@@ -2,7 +2,9 @@ import unittest
 
 from BPTK_Py import Model
 
-from BPTK_Py.sddsl.element import Element
+from BPTK_Py.sddsl.element import Element, ElementError
+from BPTK_Py.sddsl.stock import Stock
+from BPTK_Py.sddsl.operators import ArrayedEquation
 
 class TestElement(unittest.TestCase):
     def setUp(self):
@@ -18,9 +20,31 @@ class TestElement(unittest.TestCase):
         self.assertEqual(element.converters,[])
         self.assertEqual(element._function_string,element.default_function_string())
         self.assertIsNone(element._equation)
-        
+
+        self.assertIsInstance(element._elements, ArrayedEquation)
+        self.assertListEqual(element._elements.equations, [])
+        self.assertEqual(element._elements._element,element) 
+
         self.assertFalse(element.arrayed)
         self.assertFalse(element.named_arrayed)
+
+    def testElementInit_with_function_string(self):
+        model = Model()
+
+        element = Element(model=model,name="testElement",function_string="1+1")
+
+        self.assertEqual(element.model,model)
+        self.assertEqual(element.name,"testElement")
+        self.assertEqual(element.converters,[])
+        self.assertEqual(element._function_string,"1+1")
+        self.assertIsNone(element._equation)
+
+        self.assertIsInstance(element._elements, ArrayedEquation)
+        self.assertListEqual(element._elements.equations, [])
+        self.assertEqual(element._elements._element,element) 
+
+        self.assertFalse(element.arrayed)
+        self.assertFalse(element.named_arrayed)                
 
     def testElement_add_arr_equation(self):
         model = Model()
@@ -61,7 +85,78 @@ class TestElement(unittest.TestCase):
 
         element = Element(model=model,name="testElement",function_string=None)   
 
-        self.assertRaises(Exception,element.__setitem__,"testKey")   
+        self.assertRaises(Exception,element.__setitem__,"testKey","testValue")   
+
+    def testElement_setup_vector_single_value(self):
+        model = Model()
+
+        stock = Stock(model=model,name="testElement")
+
+        stock.setup_vector(size=2,default_value=1.0,set_stack_equation=False)
+
+        for i in range(2):
+            #self[i] = None can not be tested
+            self.assertEqual(stock[i].initial_value,1.0)
+
+    def testElement_setup_vector_multiple_value(self):
+        model = Model()
+
+        stock = Stock(model=model,name="testElement")
+
+        stock.setup_vector(size=2,default_value=[2.0, 3.0],set_stack_equation=False)
+
+        self.assertEqual(stock[0].initial_value,2.0)
+        self.assertEqual(stock[1].initial_value,3.0)
+
+    def testElement_setup_vector_execption(self):
+        model = Model()
+
+        stock = Stock(model=model,name="testElement")
+
+        self.assertRaises(Exception,stock.setup_vector,size=2,default_value=["testString"],set_stack_equation=False)    
+
+    def testElement_setup_named_vector(self):
+        model = Model()
+
+        stock = Stock(model=model,name="testElement")
+
+        stock.setup_named_vector(values={0 : 3.0, 1 : 4.0, 2 : 5.0},set_stack_equation=False)  
+
+        self.assertEqual(stock[0].initial_value,3.0)  
+        self.assertEqual(stock[1].initial_value,4.0)
+        self.assertEqual(stock[2].initial_value,5.0)      
+
+    def testElement_setup_matrix_exception(self):
+        model = Model()
+
+        stock = Stock(model=model,name="testElement")
+
+        self.assertRaises(Exception,stock.setup_matrix,size=1,default_value=0.0)         
+        self.assertRaises(Exception,stock.setup_matrix,size=[1],default_value=0.0)         
+        self.assertRaises(Exception,stock.setup_matrix,size=[1,2,3],default_value=0.0)         
+        
+    def testElement_setup_named_matrix_exception(self):
+        model = Model()
+
+        stock = Stock(model=model,name="testElement")    
+
+        self.assertRaises(Exception,stock.setup_named_matrix,names=1)
+        self.assertRaises(Exception,stock.setup_named_matrix,names="string")
+        self.assertRaises(Exception,stock.setup_named_matrix,names=True)
+
+class TestElementError(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testElementErrorInit(self):
+        elementError = ElementError(value="testValue")
+
+        self.assertEqual(elementError.value,"testValue")
+
+    def testElementError_str_(self):
+        elementError = ElementError(value=123)        
+
+        self.assertEqual(elementError.__str__(),"123")
 
 if __name__ == '__main__':
     unittest.main()
