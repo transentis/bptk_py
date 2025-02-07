aca401aa71c59769450702c0d58c23d293c9a6ca
mbaehring <markus.baehring@transentis.com>
2025-02-07 11:53:59 +0100

    added unittests for scenario_manager_factory

--- tests/unittests/test_scenario_manager_factory/test_scenario_manager_factory.py
+++ tests/unittests/test_scenario_manager_factory/test_scenario_manager_factory.py
@@ -1,7 +1,9 @@
 import unittest
-from unittest.mock import patch, mock_open
+from unittest.mock import mock_open, patch
 from BPTK_Py.scenariomanager.scenario_manager_factory import ScenarioManagerFactory
 import os, json
+from BPTK_Py import Model
+from  BPTK_Py.scenariomanager.scenario import SimulationScenario
 import BPTK_Py.logger.logger as logmod
 
 
@@ -85,6 +87,99 @@ class TestScenarioManagerFactory(unittest.TestCase):
         self.assertEqual(sm.get_scenarios(scenario_managers=["smPortfolio1"], scenarios=["scenarioHighInterest"])["scenarioHighInterest"].name, "scenarioHighInterest")        
 
         self.assertEqual(sm.get_scenarios(scenario_managers=["smPortfolio1"], scenarios=["scenarioHighInitialValue"]),{})          
+
+
+    def test_add_scenario_existing_manager(self):
+        #cleanup logfile
+        try:
+            with open(logmod.logfile, "w", encoding="UTF-8") as file:
+                pass
+        except FileNotFoundError:
+            self.fail()
+
+        currentDir = os.path.abspath(os.getcwd())
+        testDir = os.path.join(currentDir,"tests","unittests","test_scenario_manager_factory","scenarios")
+
+        sm = ScenarioManagerFactory(start_model_monitor=False, start_scenario_monitor=False)
+
+        sm.get_scenario_managers(path=testDir)  
+
+        dictionary = {
+            "constants": {
+                "interestRate": 0.99
+            }            
+        }
+
+        addScenario = SimulationScenario(dictionary=dictionary,name="addScenario", model=Model(), scenario_manager_name="smPortfolio1")    
+
+        sm.add_scenario(scenario=addScenario,scenario_manager="smPortfolio1")  
+
+        try:
+            with open(logmod.logfile, "r", encoding="UTF-8") as file:
+                content = file.read()
+        except FileNotFoundError:
+            self.fail()
+
+        self.assertIn("[WARN] Model Manager already existing. Not overwriting the model!", content)  
+        self.assertEqual(sm.scenario_managers["smPortfolio1"].scenarios["addScenario"].get_property_value(name="interestRate"),0.99)    
+
+    def test_add_scenario_not_existing_manager(self):
+        #cleanup logfile
+        try:
+            with open(logmod.logfile, "w", encoding="UTF-8") as file:
+                pass
+        except FileNotFoundError:
+            self.fail()
+
+        currentDir = os.path.abspath(os.getcwd())
+        testDir = os.path.join(currentDir,"tests","unittests","test_scenario_manager_factory","scenarios")
+
+        sm = ScenarioManagerFactory(start_model_monitor=False, start_scenario_monitor=False)
+
+        sm.get_scenario_managers(path=testDir)  
+
+        dictionary = {
+            "constants": {
+                "interestRate": 0.99
+            }            
+        }
+
+        currentDir = os.path.abspath(os.getcwd())
+        modelFile = os.path.join(currentDir,"tests","unittests","test_scenario_manager_factory","simulation_models","simulation_model")        
+
+        addScenario = SimulationScenario(dictionary=dictionary,name="addScenario", model=Model(), scenario_manager_name="addManager")    
+
+        sm.add_scenario(scenario=addScenario,scenario_manager="addManager", model=modelFile)  
+
+        try:
+            with open(logmod.logfile, "r", encoding="UTF-8") as file:
+                content = file.read()
+        except FileNotFoundError:
+            self.fail()
+
+        self.assertNotIn("[WARN] Model Manager already existing. Not overwriting the model!", content)  
+        self.assertEqual(sm.scenario_managers["addManager"].scenarios["addScenario"].get_property_value(name="interestRate"),0.99)   
+
+    def test_create_scenario(self):
+        sm = ScenarioManagerFactory(start_model_monitor=False, start_scenario_monitor=False)
+
+        test_filename = "test.json"
+        test_data = {
+            "constants": {
+                "interestRate": 0.001
+            }            
+        }
+
+        mock_file = mock_open()
+
+        with patch("builtins.open", mock_file):
+            sm.create_scenario(filename=test_filename, dictionary=test_data)
+
+            mock_file.assert_called_once_with(test_filename, "w", encoding="utf-8")
+
+            content = "".join(call.args[0] for call in mock_file().write.call_args_list)
+
+            self.assertEqual(json.loads(content),test_data)
 
 
 if __name__ == '__main__':
